1ee1e14ce3826e216880b8b6b4e6c91eb014ba82
oparabenjamin <oparaben1@gmail.com>
2021-10-03 00:13:20 +0100

    changed @NotNull to @NotBlank

--- src/main/java/com/tacos/Taco.java
+++ src/main/java/com/tacos/Taco.java
@@ -9,7 +9,7 @@ package com.tacos;
 import lombok.Data;
 
 import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import java.util.Date;
 import java.util.List;
@@ -17,8 +17,8 @@ import java.util.List;
 @Data
 public class Taco {
 
-    @NotNull
-    @Size(min = 5, message = "Name must be at least 5 characters long")
+    @NotBlank
+    @Size(min = 3, message = "Name must be at least 5 characters long")
     private String name;
 
     @Size(min = 1, message = "You must choose at least 1 ingredient")
